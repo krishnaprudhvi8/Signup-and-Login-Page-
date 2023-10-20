82831272fb0a0d48a02c053054f86d73a7b1e835
krishnaprudhvi8 <136586341+krishnaprudhvi8@users.noreply.github.com>
2023-10-20 11:24:28 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Register from './Register';
+import Login from './Login';
+
+function App() {
+
+  return (
+    <main className="App">
+      <Login />
+      <Register/>
+    </main>
+  );
+}
+
+export default App;
